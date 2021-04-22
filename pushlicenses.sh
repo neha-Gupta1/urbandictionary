872aee7fab3186ef9e86cb74267090749318c509
Eshan Kaley <kaleyeshan@gmail.com>
2021-04-22 09:26:30 +0530

    Update pushlicenses.sh

--- pushlicenses.sh
+++ pushlicenses.sh
@@ -8,27 +8,28 @@ host="https://dev-secops-core.multicloud-ibm.com"
 token="Nh-UYsQoL-2i4rhyEQrAm-ZUWjex4DOdR4F4UIewBKvR1TegDkpv0HLa88BdH6Rp"
 allowed='"allowed"'
 denied='"denied"'
-branch=$TRAVIS_BRANCH
+branch=$(echo "\"$TRAVIS_BRANCH\"")
 repo=$TRAVIS_REPO_SLUG
 # TRAVIS_COMMIT=12344
+
 postToDevopsIntelligence() {
 	echo $branch "\"$branch\"" "\"$repo\""
-# 	CODE=$(curl --location --request POST -sSL -w '%{http_code}' ''"$1"'/dash/api/dev_secops/v1/services/testLicense/licenses?scannedBy=license_finder' \
-#  	--header 'Authorization: Token '"$2"'' \
-# 	--header 'Content-Type: application/json' \
-# 	--data-raw '{
-# 			"license_name": '"$4"',
-#    			"status": '"$3"',
-#     	    "href":"github.com/urbandictionary",
-# 		    "endpoint_hostname":"github.com",
-# 		    "commit":'"$branch"'
-# 			}' -k)
-#     if [[ "$CODE" == *"200"* ]]; then
-#     # server return 2xx response
-#         echo $CODE
-#     else
-#         echo "possibly a duplicate record response :: $CODE"
-#     fi
+	CODE=$(curl --location --request POST -sSL -w '%{http_code}' ''"$1"'/dash/api/dev_secops/v1/services/'"$repo"'/licenses?scannedBy=license_finder' \
+ 	--header 'Authorization: Token '"$2"'' \
+	--header 'Content-Type: application/json' \
+	--data-raw '{
+			"license_name": '"$4"',
+   			"status": '"$3"',
+    	    "href":"github.com/urbandictionary",
+		    "endpoint_hostname":"github.com",
+		    "commit":'"$branch"'
+			}' -k)
+    if [[ "$CODE" == *"200"* ]]; then
+    # server return 2xx response
+        echo $CODE
+    else
+        echo "possibly a duplicate record response :: $CODE"
+    fi
 }
 
 jq -r '.dependencies|keys[]' license.json | while read key ; do
