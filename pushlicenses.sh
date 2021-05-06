9aef04885553096b1bd54b390454413dc37944a6
Eshan Kaley <kaleyeshan@gmail.com>
2021-05-06 15:31:28 +0530

    Update pushlicenses.sh

--- pushlicenses.sh
+++ pushlicenses.sh
@@ -4,54 +4,90 @@
 # sed -i '1d' license.json
 
 # variables
-host="https://secops-core.multicloud-ibm.com"
-token="jBmBPGgtilvJ76-GfBgZgCBXr1lnS8xbdeMScUSpMHMpXVMprZR2nbCn8-5zH9zx"
-allowed='"allowed"'
-denied='"denied"'
+host="https://dev-secops-core.multicloud-ibm.com"
+token="Nh-UYsQoL-2i4rhyEQrAm-ZUWjex4DOdR4F4UIewBKvR1TegDkpv0HLa88BdH6Rp"
 branch=$(echo "\"$TRAVIS_BRANCH\"")
 repo=$(echo "\"$TRAVIS_REPO_SLUG\"")
 # TRAVIS_COMMIT=12344
 
+# we need to find a way  to populate these arrays(allowed & denied), for now just hard coding them
+allowed=("MIT")
+denied=("New BSD" "\"Apache 2.0,MIT\"")
+
+
 postToDevopsIntelligence() {
-# 	echo $branch "\"$branch\"" "\"$repo\""
-# 	CODE=$(
-	curl --location --request POST -sSL -w '%{http_code}' ''"$1"'/dash/api/dev_secops/v1/services/UrbanDictionary/licenses?scannedBy=license_finder' \
+	echo $branch "\"$branch\"" "\"$repo\""
+	CODE=$(curl --location --request POST -sSL -w '%{http_code}' ''"$1"'/dash/api/dev_secops/v1/services/newTestLicense/licenses?scannedBy=license_finder' \
  	--header 'Authorization: Token '"$2"'' \
 	--header 'Content-Type: application/json' \
-	--data-raw '{
-			"license_name": '"$4"',
-   			"status": '"$3"',
-    	    "href":"github.com/urbandictionary",
-		    "endpoint_hostname":"github.com",
-		    "commit":'"$branch"'
-			}' -k
-
-# 			)
-#     if [[ "$CODE" == *"200"* ]]; then
-#     # server return 2xx response
-#         echo $CODE
-#     else
-#         echo "possibly a duplicate record response :: $CODE"
-#     fi
+	--data-raw "'$3'" -k)
+    if [[ "$CODE" == *"200"* ]]; then
+    # server return 2xx response
+        echo "Posted License Scan Results to DevOps Intelligence Successfully :: "$CODE
+    else
+        echo "Error while posting License Scan Results to DevOps Intelligence :: $CODE"
+    fi
 }
 
-jq -r '.dependencies|keys[]' license.json | while read key ; do
-    jqCmd=$(cat license.json | jq '.dependencies['$key'].licenses | map(.) |join(",")' )
-    licenses="$jqCmd"
-    status="$(jq '.dependencies['$key'].approved' license.json)"
-    # printf "%s\t%s\t%s\t%s\n" "$name" "$version" "$licenses" "$allowed"
-    # echo $allowed
-    # echo "$allowed"
-    # echo '"Not Approved"'
-    case $status in 
-      '"Approved"')
-        # echo "OK"
-        postToDevopsIntelligence $host $token $allowed "$licenses"
-        ;;
 
-      *)
-        # echo "NOTOK"
-        postToDevopsIntelligence $host $token $denied "$licenses"
-        ;;
-    esac    
+# list=$(license_finder --prepare-no-fail --format=json --no-recursive --no-debug | awk '!/^[A-Z]/' | jq '.dependencies[].licenses[]')
+listcsv=$(license_finder --prepare-no-fail --format=csv --no-recursive --no-debug | awk '!/^[A-Z]/' > ll.csv)
+
+
+arr=()
+
+while IFS="," read -r rec_column1 rec_column2 rec_column3 
+do
+  arr+=("$rec_column3")
+done < <(tail -n +2 ll.csv)
+
+echo "Done"
+echo "Allowed Licenses : "${allowed[@]}
+echo "Denied Denied : "${denied[@]}
+
+# uniques=($(for v in "${arr[@]}"; do echo "'$v'";done| sort| uniq| xargs))
+
+IFS=$'\n' uniques=(`printf "%s\n" "${arr[@]}" |sort -u`)
+
+# uniques=($(printf "%s\n" "${arr[@]}" | sort -u))
+
+echo "List of Unique Licenses found in the Project~" 
+echo "------------------"
+for v in "${uniques[@]}";do
+  echo "$v"
 done
+echo "------------------"
+
+all=('[]') #this array will hold the payload to be sent to DevOps Intelligence
+for i in "${uniques[@]}"; do # traverse through the uniques and check if they are allowed, denied or uncategorized.
+  if [[ "${allowed[*]}" == *"$i"* ]]; then
+    # echo $i is allowed
+        payload=$( jq -n \
+                  --arg ln "$i" \
+                  --arg ad "$(date +%FT%T.%3NZ)" \
+                  --arg st "allowed" \
+                  '{name: $ln, analysis_date: $ad, status: $st}' )
+        all=$(echo $all | jq ".+=[$payload]")
+  elif [[ "${denied[*]}" == *"$i"* ]]; then
+    # echo $i is denied
+        payload=$( jq -n \
+                  --arg ln "$i" \
+                  --arg ad "$(date +%FT%T.%3NZ)" \
+                  --arg st "denied" \
+                  '{license_name: $ln, analysis_date: $ad, status: $st}' )
+        all=$(echo $all | jq ".+=[$payload]")
+  else 
+    # echo $i is uncategorized
+        payload=$( jq -n \
+                  --arg ln "$i" \
+                  --arg ad "$(date +%FT%T.%3NZ)" \
+                  --arg st "uncategorized" \
+                  '{license_name: $ln, analysis_date: $ad, status: $st}' )
+        all=$(echo $all | jq ".+=[$payload]")
+  fi
+done
+
+echo payload
+echo ${all[0]}
+
+# postToDevopsIntelligence $host $token ${all[0]}
